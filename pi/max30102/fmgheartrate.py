aa80dfbec320b63f262d24b5c4e9b249f09b32fe
Phichael <117380811+Phichael@users.noreply.github.com>
2022-12-13 20:15:27 -0600

    Update fmgheartrate.py

--- pi/max30102/fmgheartrate.py
+++ pi/max30102/fmgheartrate.py
@@ -1,11 +1,8 @@
-import matplotlib.pyplot as plt
 import statistics
-
-inputFileName = "bpm_data.txt"
 
 def main():
     # read file
-    file = open(inputFileName)
+    file = open("miguel_hr.txt")
     lines = file.readlines()
     file.close()
 
@@ -28,14 +25,15 @@ def main():
     
     oneHR = statistics.mode(hr)
     print(oneHR-40)
-    # print(hr)
-    xaxis = list(range(1,325))
-    # print(xaxis)
-    plt.plot(list(range(1,325)),hr)
-    plt.xlabel('Sample')
-    plt.ylabel('Heart Rate (Beats per Minute)')
-    plt.title('Heart Rate vs. Sample')
-    plt.show()
+
+    # # print(hr)
+    # xaxis = list(range(1,325))
+    # # print(xaxis)
+    # plt.plot(list(range(1,325)),hr)
+    # plt.xlabel('Sample')
+    # plt.ylabel('Heart Rate (Beats per Minute)')
+    # plt.title('Heart Rate vs. Sample')
+    # plt.show()
 
     # plt.plot([1,2,3,4], [4,3,2,5])
     # plt.show()
